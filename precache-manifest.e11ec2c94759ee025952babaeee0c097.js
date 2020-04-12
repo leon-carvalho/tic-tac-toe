c3b42aa630568cecc0f3bdd2c72c9e0d75770794
leon-carvalho <leonardoscc15@gmail.com>
2020-04-12 13:47:17 -0300

    Updates

---
+++ precache-manifest.e11ec2c94759ee025952babaeee0c097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3ea1e15fbe479ef6451a9d30c1d572",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8e9c0069093be6e46372",
+    "url": "/tic-tac-toe/static/js/2.e41149c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.e41149c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7219eca45be27e6b6daa",
+    "url": "/tic-tac-toe/static/js/main.11a5d3e1.chunk.js"
+  },
+  {
+    "revision": "28dfba9420967d301740",
+    "url": "/tic-tac-toe/static/js/runtime-main.4c12a8d1.js"
+  }
+]);
